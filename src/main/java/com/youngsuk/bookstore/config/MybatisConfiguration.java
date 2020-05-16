b84a7ff1f8e48c80c13943e752a64084a96ea1eb
youngsuk-dev <63249033+youngsuk-dev@users.noreply.github.com>
2020-05-16 17:39:19 +0900

    안쓰는 import 코드 삭제

--- src/main/java/com/youngsuk/bookstore/config/MybatisConfiguration.java
+++ src/main/java/com/youngsuk/bookstore/config/MybatisConfiguration.java
@@ -6,8 +6,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
