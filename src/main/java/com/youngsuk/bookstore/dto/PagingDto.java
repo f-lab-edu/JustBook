52626a493303944ca8170495931cac1925a09cb4
youngsuk-dev <63249033+youngsuk-dev@users.noreply.github.com>
2020-08-16 10:16:52 +0900

    [#10] PagingDto 클래스가 불변이 아닌 이유 주석으로 추가

--- src/main/java/com/youngsuk/bookstore/dto/PagingDto.java
+++ src/main/java/com/youngsuk/bookstore/dto/PagingDto.java
@@ -6,7 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 /*
- * 해당 class는 @setter 때문에 불변 객체가 아니다.
+ * 해당 class는 @Setter 때문에 불변 객체가 아니다.
+ * @Setter는 setter함수를 public으로 만들기 때문에 setter가 적용된 인스턴스 변수를 다른 사용자가 변경할 수 있게 한다.
  */
 public final class PagingDto  {
   private int onePageLimit;
