98ff94059ddf83546895d2f9db9276f693a94eaf
thinker-94 <thinker-94@gmail.com>
2020-08-21 13:02:55 +0900

    Revert "[#27] userQuery.xml의 조건문 수정"
    This reverts commit dcbebe89c62599ceb41470511cc9565c4fb8bacf.

--- src/main/java/com/youngsuk/bookstore/common/utils/constants/SortTypeEnum.java
+++ src/main/java/com/youngsuk/bookstore/common/utils/constants/SortTypeEnum.java
@@ -3,18 +3,10 @@ package com.youngsuk.bookstore.common.utils.constants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
+@Getter
 public enum SortTypeEnum {
   Newest("00"), Likes("01");
 
   private final String code;
-
-  public SortTypeEnum getNewest() {
-    return Newest;
-  }
-
-  public SortTypeEnum getLikes() {
-    return Likes;
-  }
 }
