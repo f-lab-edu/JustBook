e29a720ab5a775b851adb10d6605c854d86f96b0
thinker-94 <thinker-94@github.com>
2020-09-19 18:37:41 +0900

    [#34] BookController giveBookByCategory() 리팩토링
    사용자의 어플리케이션 이용 흐름대로 컨트롤러 메소드 리팩토링

--- src/main/java/com/youngsuk/bookstore/controller/BookController.java
+++ src/main/java/com/youngsuk/bookstore/controller/BookController.java
@@ -24,22 +24,44 @@ public class BookController {
   private BookService bookService;
 
   /**
-   * @return 모든 책의 카테고리를 리턴한다 ex) 인문학, 철학 ..
+   *  @return 모든 책의 코드, 카테고리 리턴
+   *    ex) 인문학, 철학 .. (카테고리는 예시용)
    */
   @GetMapping
   public List<BookCategoryDto> getCategoryList() {
     return bookService.cacheBookCategoryList();
   }
 
+  /**
+   * @param 사용자가 카테고리 목록에서 클릭한 카테고리의 코드를 받는다
+   *             ex) 인문학 001, 철학 002 (코드는 설명용)
+   * @return 해당 카테고리의 책 목록을 디폴트 정렬 방법에 맞게 리턴
+   */
   @GetMapping
-  public List<BookDto> getBookByCategoryName() {
-    //todo defalut 최신순
+  public List<BookDto> getBookByCategory(String code) {
+    return bookService.choiceDefaultBookSortType(code);
   }
 
+  /**
+   * @param 사용자가 변경한 카테고리 정렬 코드를 받는다
+   *             ex) 판매순 001, 인기순 002 (코드는 설명용)
+   * @return 해당 카테고리의 책 목록을 정렬 코드에 따라 다르게 리턴
+   */
   @GetMapping
-  public List<BookDto> getBookBySortType() {
-    //todo
+  public List<BookDto> getChangedSortType(String code) {
+    return bookService.cacheBookListBySortType(code);
   }
+
+  /**
+   * @param 사용자가 선택한 페이지의 숫자를 받는다
+   *             ex) 1,2,3,4,5 ...
+   * @return 해당 카테고리의 책 목록을 정렬 코드에 따라 다르게 리턴
+   */
+  @GetMapping
+  public List<BookDto> getChangedPageNumber(String page) {
+    return bookService.onePageItemCount(page);
+  }
+}
 
 
 
