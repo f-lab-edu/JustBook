7065e7d983b8b03f1bb55307c24fd5932b973504
thinker <thinker-94@gmail.com>
2020-09-26 21:49:18 +0900

    [#38] 스프링 MVC에서 사용하는 용어에 맞추기 위해 DAO에서 Repository로 클래스, 패키지명 수정

--- src/main/java/com/youngsuk/bookstore/repository/BookRepository.java
+++ src/main/java/com/youngsuk/bookstore/repository/BookRepository.java
@@ -1,14 +1,18 @@
 package com.youngsuk.bookstore.repository;
 
-import com.youngsuk.bookstore.dto.BookDto;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
+/**
+ * 책 데이터를 저장하기 위한 기능이 있는 인터페이스
+ *
+ * @param <T> 책 도메인과 관련된 Type
+ * @param <Id> Id를 가지고 있는 POJO class //TODO. Id 클래스 생성
+ */
+public interface BookRepository<T, Id> {
 
-@Mapper
-@Repository
-public interface BookRepository {
-  List<BookDto> selectBookByCategory(BookDto bookDto);
-  List<BookDto> findBookCategoryList();
+    //카테고리 목록 전체 조회에 사용
+    List<T> findAll();
+
+    //책 상세 정보 조회에 사용
+    Id findById(Id id);
 }
